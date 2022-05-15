fcc964f87c4c09f6edfc4133565d3c2d9852afaa
Cosmin2410 <cosminnae00@yahoo.com>
2022-05-15 17:14:13 +0300

    shuffle done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import Questions from './Questions';
-import Answers from './Answers';
+// import Answers from './Answers';
 
 function App() {
   const [apiQues, setApiQues] = useState([]);
-  // const [changeQuiz, setChangeQuiz] = useState(0);
-
-  // const test = console.log(true);
+  const [nextQues, setNextQues] = useState(
+    'https://opentdb.com/api.php?amount=1&category=18&type=multiple'
+  );
 
   useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=1&category=18&type=multiple')
       .then((res) => res.json())
       .then((data) => setApiQues(data.results));
-  }, []);
+  }, [nextQues]);
 
-  const getQuestions = apiQues.map((ques) => (
-    <Questions
-      key={ques.question}
-      value={ques.question}
-      incorrectAnswers={ques.incorrect_answers}
-      correctAnswer={ques.correct_answer}
-    />
-  ));
+  console.log(true);
+
+  const getQuestions = apiQues.map((ques) => {
+    // Shuffle answers
+
+    let shuffle = [ques.correct_answer, ...ques.incorrect_answers].sort(
+      () => Math.random() - 0.5
+    );
+
+    return (
+      <Questions
+        key={ques.question}
+        value={ques.question}
+        // incorrectAnswers={ques.incorrect_answers}
+        correctAnswer={ques.correct_answer}
+        shuffle={shuffle}
+      />
+    );
+  });
 
   return (
     <div className="quizz-app">
       {getQuestions}
-      <button>click</button>
+      <button onClick={setNextQues}>click</button>
     </div>
   );
 }
--- src/Questions.js
+++ src/Questions.js
@@ -1,31 +1,16 @@
 import React from 'react';
 
 function Questions(props) {
-  // Shuffle answers
+  console.log(`%c ${props.correctAnswer}`, 'color: blue; ');
 
-  // function shuffle() {
-  //   const answers = document.querySelectorAll('.answer');
-  //   answers.forEach((answer) => {
-  //     let randomNum = Math.floor(Math.random() * 4);
-  //     answer.style.order = randomNum;
-  //   });
-  // }
-
-  let shuffle = [props.correctAnswer, ...props.incorrectAnswers];
-  console.log(shuffle);
-
-  shuffle = shuffle.sort(() => Math.random() - 0.5);
-  console.log(shuffle);
-
-  // dangerouslySetInnerHTML={{ __html: props.correctAnswer }}
   return (
     <div>
       <h1 dangerouslySetInnerHTML={{ __html: props.value }} />
       <div className="answers">
-        <button className="answer">{shuffle[0]}</button>
-        <button className="answer">{shuffle[1]}</button>
-        <button className="answer">{shuffle[2]}</button>
-        <button className="answer">{shuffle[3]}</button>
+        <p dangerouslySetInnerHTML={{ __html: props.shuffle[0] }} />
+        <p dangerouslySetInnerHTML={{ __html: props.shuffle[1] }} />
+        <p dangerouslySetInnerHTML={{ __html: props.shuffle[2] }} />
+        <p dangerouslySetInnerHTML={{ __html: props.shuffle[3] }} />
       </div>
     </div>
   );
